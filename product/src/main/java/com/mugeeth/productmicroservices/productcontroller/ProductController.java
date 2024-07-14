45b844ccda820d2dfd4bc31af86d8d1405a25439
MUGEETH <mugeeth02@gmail.com>
2024-07-14 22:20:54 +0530

    raja anna test

--- product/src/main/java/com/mugeeth/productmicroservices/productcontroller/ProductController.java
+++ product/src/main/java/com/mugeeth/productmicroservices/productcontroller/ProductController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProductController {
+public class ProductController { //Test done by raja anna
     @GetMapping("/products") //http:localhost:8082/products
     public String products(){
         return "PRODUCTS";
